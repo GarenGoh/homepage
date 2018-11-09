7f45acf34e5724a9ddd7796537f41cb0736f6160
garen <188226814@qq.com>
2018-11-09 18:43:40 +0800

    Update

--- controllers/WxController.php
+++ controllers/WxController.php
@@ -9,6 +9,7 @@ class WxController extends BaseController
     public function actionEvent()
     {
         $get = Yii::$app->request->getQueryParams();
+        $post = Yii::$app->request->getBodyParams();
 
         $signature = $get['signature'];
         $echostr = $get['echostr'];
@@ -16,7 +17,8 @@ class WxController extends BaseController
         $nonce = $get['nonce'];
         $token = '9292';
 
-        AppHelper::log('test', '$echostr', $echostr);
+        AppHelper::log('test', '$get', $get);
+        AppHelper::log('test', '$post', $post);
 
         return $echostr;
     }
